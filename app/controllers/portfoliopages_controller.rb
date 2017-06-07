6b955a449baa38b8c8e9e4e65e15dcf0aa3761ad
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2017-06-07 18:39:52 +0200

    New and created actions for portfolio

--- app/controllers/portfoliopages_controller.rb
+++ app/controllers/portfoliopages_controller.rb
@@ -2,4 +2,21 @@ class PortfoliopagesController < ApplicationController
   def index
     @portfolio_items = Portfoliopage.all
   end
+
+  def new
+    @portfolio_item = Portfoliopage.new
+  end
+
+  def create
+    @portfolio_item = Portfoliopage.new(params.require(:portfoliopage).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfoliopages_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
