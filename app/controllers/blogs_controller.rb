64683ddf605ff59668707502cdf86fbbe3025e8c
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2017-06-08 23:24:06 +0200

    Friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -62,7 +62,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])#ranije je trazio broj tj. id. Friendly trazi params ali mapuje slug za id i nalazi ga.
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged #generise blog title
 end
