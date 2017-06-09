a7b491db655e1b8e40ae1e3f52d41b2ecd8a9284
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2017-06-09 17:17:41 +0200

    Updated validations for blogs, skills and portfolio features

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1}#vrednosti za draft i published blog
   extend FriendlyId
-  friendly_id :title, use: :slugged #generise blog title
+  friendly_id :title, use: :slugged #generise blog title link
+
+  validates_presence_of :title, :body#ne dozvoljava da se napravi blog ako nema title i body
 end
