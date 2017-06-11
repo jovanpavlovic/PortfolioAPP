02437fd9ea1b12f2ff4637a1be3c0191b9540870
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2017-06-11 17:11:56 +0200

    Self Name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    puts self.name#self.name - ime user-a, split uzima string i deli ime u 2 dela first ime, last prezime Hi, John!
+  end
+
+  def last_name
+    puts self.name
+  end
 end
+
+
+
