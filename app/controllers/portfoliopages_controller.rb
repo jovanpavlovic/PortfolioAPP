9717115c6eac779dd4bbb4e9055423c5107e35fa
Jovan Pavlovic <jovan94pavlovic@gmail.com>
2017-06-22 00:05:59 +0200

    carrierwave installation

--- app/controllers/portfoliopages_controller.rb
+++ app/controllers/portfoliopages_controller.rb
@@ -30,7 +30,7 @@ class PortfoliopagesController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfoliopages_path, notice: 'Your portfolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
